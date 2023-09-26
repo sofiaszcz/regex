cf62cb0197f667f9f3fc656d34fa535a8101e8fd
sofiaszczepaniak <szczepaniak.sofia@gmail.com>
2023-09-26 19:36:05 +0200

    Refactor Regex002

--- src/Regex002.java
+++ src/Regex002.java
@@ -2,7 +2,13 @@ import regex.RegexTester;
 
 public class Regex002 {
     public static void main(String[] args) {
-        RegexTester tester = new RegexTester("[A-Z]{1}[a-z]*");
-
+        RegexTester tester = new RegexTester("[A-Z][a-z]+");
+        tester.validate("Java");//true
+        tester.validate("Ja");//true
+        tester.validate("java");//false
+        tester.validate("javA");//false
+        tester.validate("JAVa");//false
+        tester.validate("J");//false
+        tester.validate("aJ");//false
     }
 }
